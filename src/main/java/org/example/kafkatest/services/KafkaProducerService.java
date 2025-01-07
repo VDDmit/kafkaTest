5f7aa2dadefd720b74a1f31d67dba4228ccacdc3
vddmit <danvoropaeff@yandex.ru>
2025-01-07 19:57:20 +0300

    ProducerService for the task

--- src/main/java/org/example/kafkatest/services/KafkaProducerService.java
+++ src/main/java/org/example/kafkatest/services/KafkaProducerService.java
@@ -1,6 +1,11 @@
 package org.example.kafkatest.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.example.kafkatest.models.Email;
+import org.example.kafkatest.models.PushMessage;
+import org.example.kafkatest.models.SMS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -10,15 +15,32 @@ import org.springframework.stereotype.Service;
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    public void sendMessage(String topic, String message) {
-        log.info("Sending message '{}' to topic '{}'", message, topic);
-        kafkaTemplate.send(topic, message);
-        log.info("Message '{}' sent to topic '{}'", message, topic);
+    public void sendEmail(String topic, Email email) {
+        sendMessage(topic, email, "email");
     }
-}
+
+    public void sendPushMessage(String topic, PushMessage pushMessage) {
+        sendMessage(topic, pushMessage, "push message");
+    }
+
+    public void sendSMS(String topic, SMS sms) {
+        sendMessage(topic, sms, "SMS");
+    }
+
+    private void sendMessage(String topic, Object messageObject, String messageType) {
+        try {
+            String message = objectMapper.writeValueAsString(messageObject);
+            log.info("Sending {} to Kafka topic '{}': {}", messageType, topic, message);
+            kafkaTemplate.send(topic, message);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize " + messageType, e);
+        }
+    }}
