9d8616f36da95fc24223f44d5fea29d2da0ae004
vddmit <danvoropaeff@yandex.ru>
2025-01-07 15:19:36 +0300

    Initial commit for learning Kafka

---
+++ src/main/java/org/example/kafkatest/controller/KafkaController.java
@@ -0,0 +1,29 @@
+package org.example.kafkatest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.kafkatest.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    @Autowired
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String topic, @RequestParam String message) {
+        log.info("Received request to send message to topic '{}'", topic);
+        kafkaProducerService.sendMessage(topic, message);
+        log.info("Message '{}' sent to topic '{}'", message, topic);
+        return "Message sent to topic: " + topic;
+    }
+}
---
+++ src/main/java/org/example/kafkatest/service/KafkaConsumerService.java
@@ -0,0 +1,15 @@
+package org.example.kafkatest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-group")
+    public void listen(String message) {
+        log.info("Received message: {}", message);
+    }
+}
