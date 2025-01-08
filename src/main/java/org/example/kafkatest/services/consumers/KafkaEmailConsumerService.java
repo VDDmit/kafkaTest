03da0cfb6d693947b94b7611f67e0803b9c33e09
vddmit <danvoropaeff@yandex.ru>
2025-01-08 14:13:47 +0300

    Added dlt sending and corrected a typo in the folder name.

--- src/main/java/org/example/kafkatest/services/consumers/KafkaEmailConsumerService.java
+++ src/main/java/org/example/kafkatest/services/consumers/KafkaEmailConsumerService.java
@@ -1,10 +1,11 @@
-package org.example.kafkatest.services.conaumers;
+package org.example.kafkatest.services.consumers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.example.kafkatest.models.Email;
 import org.example.kafkatest.repositories.EmailRepository;
+import org.example.kafkatest.services.dlt.DLTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,13 @@ import org.springframework.stereotype.Service;
 public class KafkaEmailConsumerService {
 
     private final EmailRepository emailRepository;
+    private final DLTService dltService;
+
 
     @Autowired
-    public KafkaEmailConsumerService(EmailRepository emailRepository) {
+    public KafkaEmailConsumerService(EmailRepository emailRepository, DLTService dltService) {
         this.emailRepository = emailRepository;
+        this.dltService = dltService;
     }
 
     @KafkaListener(topics = "emails", groupId = "my-group")
@@ -28,6 +32,7 @@ public class KafkaEmailConsumerService {
             log.info("Email saved to database: {}", email);
         } catch (JsonProcessingException e) {
             log.error("Failed to deserialize email message", e);
+            dltService.sendToDLT("email",message,e);
         }
     }
 }
